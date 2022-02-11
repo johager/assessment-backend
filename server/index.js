43ab72dc62aaf6bc546296087ca33324e6f004cd
James Hager <james@avantiapplications.com>
2022-02-11 11:43:43 -0700

    Initial capabilities completed

--- server/index.js
+++ server/index.js
@@ -2,24 +2,15 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
-
+const ctrl = require('./controller.js')
 
 app.use(cors());
-
 app.use(express.json()); // When we want to be able to accept JSON.
 
-app.get("/api/compliment", (req, res) => {
-  const compliments = ["Gee, you're a smart cookie!",
-					 "Cool shirt!",
-					 "Your Javascript skills are stellar.",
-  ];
-
-  // choose random compliment
-  let randomIndex = Math.floor(Math.random() * compliments.length);
-  let randomCompliment = compliments[randomIndex];
-
-  res.status(200).send(randomCompliment);
-  
-});
+app.get("/api/compliment", ctrl.getCompliment)
+app.get("/api/fortune", ctrl.getFortune)
+app.get("/api/novel", ctrl.getNovel)
+app.get("/api/fugitive", ctrl.getFugitive)
+app.post("/api/doCalcOperation", ctrl.doCalcOperation)
 
 app.listen(4000, () => console.log("Server running on 4000"));
